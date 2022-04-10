b440c8113e679ddf8aae6a5ee8354d63d54a797b
lemon-puls <103344043+lemon-puls@users.noreply.github.com>
2022-04-10 15:18:06 +0800

    pull commit

--- src/main/java/com/example/git_demo01/GitTest.java
+++ src/main/java/com/example/git_demo01/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("master hello,git02444444");
         System.out.println("hot-fix hello,git03333!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
