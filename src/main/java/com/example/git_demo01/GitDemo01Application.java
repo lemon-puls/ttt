e8a200828d21c655864407c93f1caac6d4b61a8f
李智维 <9794454+lemon_plus@user.noreply.gitee.com>
2022-04-10 12:47:49 +0800

    first commit01

---
+++ src/main/java/com/example/git_demo01/GitDemo01Application.java
@@ -0,0 +1,13 @@
+package com.example.git_demo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemo01Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/git_demo01/GitTest.java
@@ -0,0 +1,7 @@
+package com.example.git_demo01;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
