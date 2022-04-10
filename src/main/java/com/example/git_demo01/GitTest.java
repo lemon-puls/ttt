906c74536404e9420a38cc3cb0025913a2c39c7d
李智维 <9794454+lemon_plus@user.noreply.gitee.com>
2022-04-10 12:51:22 +0800

    second commit01

--- src/main/java/com/example/git_demo01/GitTest.java
+++ src/main/java/com/example/git_demo01/GitTest.java
@@ -3,5 +3,6 @@ package com.example.git_demo01;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("hello,git01!");
     }
 }
